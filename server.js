e7f4a8248011b8bcea98e83554fa6f286f6d402e
Salvador Beltran <salbeltran1995@gmail.com>
2022-07-21 11:54:49 -0400

    votes table

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const inputCheck = require('./utils/inputCheck');
 const db = require('./db/connection');
+const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-// Add near the top of the file
-const apiRoutes = require('./routes/apiRoutes');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
